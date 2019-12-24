1c7ca0b49b2f2cb95e73d328633ee787b0ea0b9d
Smartman Apps <20635275+SmartmanApps@users.noreply.github.com>
2019-12-24 16:45:08 +1100

    Add project files.

---
+++ ColourSettings/ColourSettingsPage.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace SmartmanApps {public class ColourSettingsPage :ContentPage {
+
+public ColourSettingsPage(){
+    Title="ColourSettingsPage";
+    BackgroundColor=Color.Black;
+    Label CSPLabel=new Label{
+        HorizontalTextAlignment=TextAlignment.Center,
+        VerticalTextAlignment=TextAlignment.Center
+        };
+    CSPLabel.TextColor=Color.FromHex("009933");
+    CSPLabel.Text="ColourSettingsPage";
+//    var MainStack=new StackLayout{VerticalOptions=LayoutOptions.Center};
+//    MainStack.Children.Add(TopLabel);
+//    Content=MainStack;
+    Content=CSPLabel;
+    }
+
+}}
