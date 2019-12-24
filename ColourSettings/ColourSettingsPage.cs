b83c4afd088f0b18350e3056aa28c7bffabfa51a
Smartman Apps <20635275+SmartmanApps@users.noreply.github.com>
2019-12-24 16:47:13 +1100

    1st version - just a label

--- ColourSettings/ColourSettingsPage.cs
+++ ColourSettings/ColourSettingsPage.cs
@@ -2,15 +2,15 @@
 
 namespace SmartmanApps {public class ColourSettingsPage :ContentPage {
 
-public ColourSettingsPage(){
-    Title="ColourSettingsPage";
+public ColourSettingsPage() {
+    Title="ColourSettingsPage Label";
     BackgroundColor=Color.Black;
     Label CSPLabel=new Label{
         HorizontalTextAlignment=TextAlignment.Center,
         VerticalTextAlignment=TextAlignment.Center
         };
     CSPLabel.TextColor=Color.FromHex("009933");
-    CSPLabel.Text="ColourSettingsPage";
+    CSPLabel.Text="ColourSettingsPage Label";
 //    var MainStack=new StackLayout{VerticalOptions=LayoutOptions.Center};
 //    MainStack.Children.Add(TopLabel);
 //    Content=MainStack;
