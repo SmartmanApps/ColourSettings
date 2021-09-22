2e459b7f653583ad90ece19ffe463a7510373aa8
Smartman Apps <20635275+SmartmanApps@users.noreply.github.com>
2021-09-23 02:37:49 +1000

    Fixed bindings and added preference saving

--- ColourSettings/ColourSettingsPage.cs
+++ ColourSettings/ColourSettingsPage.cs
@@ -1,74 +1,89 @@
 ﻿using SmartmanApps;
 using Amporis.Xamarin.Forms.ColorPicker;
 using Xamarin.Forms;
+using Xamarin.Essentials;
+
 namespace SmartmanApps {public class ColourSettingsPage :ContentPage {
 #region variables
-IColours Colours;
 public ColorPickerEntry AccentColourEntry,BgColourEntry,FgColourEntry,WarnColourEntry;
 public FontAttributes FontProperty=FontAttributes.Bold;
 public Grid ColourSettingsGrid;
 public Label AccentLabel,BgLabel,FgLabel,WarnLabel;
 #endregion
 #region constructor
-public ColourSettingsPage(IColours colours)
+public ColourSettingsPage(IColours Colours)
 {
 Padding=new Thickness(30,20,30,0);
-Colours=colours;
 Title="ColourSettings Page";
-this.SetBinding(BackgroundColorProperty,new Binding(nameof(Colours.Background),source:Colours));
 #region views
 #region ColourEntries
-AccentColourEntry=new ColorPickerEntry{EditAlfa=false,BackgroundColor=Color.White,};
-BgColourEntry=new ColorPickerEntry{EditAlfa=false,BackgroundColor=Color.White,};
-FgColourEntry=new ColorPickerEntry{EditAlfa=false,BackgroundColor=Color.White,};
-WarnColourEntry=new ColorPickerEntry{EditAlfa=false,BackgroundColor=Color.White,};
-AccentColourEntry.Color=Colours.Accent;
-BgColourEntry.Color=Colours.Background;
-FgColourEntry.Color=Colours.Foreground;
-WarnColourEntry.Color=Colours.Warning;
-AccentColourEntry.SetBinding(ColorPickerEntry.ColorProperty,new Binding(nameof(Colours.Accent),source:Colours));
-BgColourEntry.SetBinding(ColorPickerEntry.ColorProperty,new Binding(nameof(Colours.Background),source:Colours));
-FgColourEntry.SetBinding(ColorPickerEntry.ColorProperty,new Binding(nameof(Colours.Foreground),source:Colours));
-WarnColourEntry.SetBinding(ColorPickerEntry.ColorProperty,new Binding(nameof(Colours.Warning),source:Colours));
+#region AccentColourEntry
+AccentColourEntry=new ColorPickerEntry{
+    EditAlfa=false,
+    BackgroundColor=Color.White,
+    Color=Colours.Accent,
+    };
+#endregion
+#region BgColourEntry
+BgColourEntry=new ColorPickerEntry{
+    EditAlfa=false,
+    BackgroundColor=Color.White,
+    Color=Colours.Background,
+    };
+#endregion
+#region FgColourEntry
+FgColourEntry=new ColorPickerEntry{
+    EditAlfa=false,
+    BackgroundColor=Color.White,
+    Color=Colours.Foreground,
+    };
+#endregion
+#region WarnColourEntry
+WarnColourEntry=new ColorPickerEntry{
+    EditAlfa=false,
+    BackgroundColor=Color.White,
+    Color=Colours.Warning,
+    };
+#endregion
 #endregion
 #region labels
-#region Accentabel
+#region AccentLabel
 AccentLabel=new Label{
     Text="Accent colour",
     FontAttributes=FontProperty,
     };
-AccentLabel.SetBinding(Label.TextColorProperty,new Binding(nameof(Colours.Foreground),source:Colours));
-AccentLabel.SetBinding(Label.BackgroundColorProperty,new Binding(nameof(Colours.Accent),source:Colours));
+AccentLabel.SetBinding(Label.TextColorProperty,new Binding(nameof(FgColourEntry.Color),source:FgColourEntry));
+AccentLabel.SetBinding(Label.BackgroundColorProperty,new Binding(nameof(AccentColourEntry.Color),source:AccentColourEntry));
 #endregion
 #region BgLabel
 BgLabel=new Label{
     Text="Background colour",
     FontAttributes=FontProperty,
     };
-BgLabel.SetBinding(Label.TextColorProperty,new Binding(nameof(Colours.Foreground),source:Colours));
-BgLabel.SetBinding(Label.BackgroundProperty,new Binding(nameof(Colours.Background),source:Colours));
+BgLabel.SetBinding(Label.TextColorProperty,new Binding(nameof(FgColourEntry.Color),source:FgColourEntry));
+BgLabel.SetBinding(Label.BackgroundProperty,new Binding(nameof(BgColourEntry.Color),source:BgColourEntry));
 #endregion
 #region FgLabel
 FgLabel=new Label{
     Text="Foreground (text) colour",
     FontAttributes=FontProperty,
     };
-FgLabel.SetBinding(Label.TextColorProperty,new Binding(nameof(Colours.Foreground),source:Colours));
-FgLabel.SetBinding(Label.BackgroundColorProperty,new Binding(nameof(Colours.Background),source:Colours));
+FgLabel.SetBinding(Label.TextColorProperty,new Binding(nameof(FgColourEntry.Color),source:FgColourEntry));
+FgLabel.SetBinding(Label.BackgroundColorProperty,new Binding(nameof(BgColourEntry.Color),source:BgColourEntry));
 #endregion
 #region WarnLabel
 WarnLabel=new Label{
     Text="Warning colour",
     FontAttributes=FontProperty,
     };
-WarnLabel.SetBinding(Label.TextColorProperty,new Binding(nameof(Colours.Warning),source:Colours));
-WarnLabel.SetBinding(Label.BackgroundColorProperty,new Binding(nameof(Colours.Background),source:Colours));
+WarnLabel.SetBinding(Label.TextColorProperty,new Binding(nameof(WarnColourEntry.Color),source:WarnColourEntry));
+WarnLabel.SetBinding(Label.BackgroundColorProperty,new Binding(nameof(BgColourEntry.Color),source:BgColourEntry));
 #endregion
 #endregion
 #endregion
 #region assemble GUI
+this.SetBinding(BackgroundColorProperty,new Binding(nameof(BgColourEntry.Color),source:BgColourEntry));
 ColourSettingsGrid=new Grid();
-//ColourSettingsGrid.ColumnDefinitions.Add(new ColumnDefinition{Width=GridLength.Auto});
 ColourSettingsGrid.ColumnDefinitions.Add(new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)});
 ColourSettingsGrid.ColumnDefinitions.Add(new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)});
 ColourSettingsGrid.RowDefinitions.Add(new RowDefinition{Height=GridLength.Auto});
@@ -86,12 +101,14 @@ ColourSettingsGrid.Children.Add(WarnColourEntry,1,3);
 #endregion
 }
 #region methods
+#region RemoveWarningColourEntry
 public void RemoveWarningColourEntry()
 {
 ColourSettingsGrid.Children.Remove(WarnLabel);
 ColourSettingsGrid.Children.Remove(WarnColourEntry);
 ColourSettingsGrid.RowDefinitions.RemoveAt(3);
 }
+#endregion
 #endregion
 #endregion
 }}
